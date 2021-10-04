04a8aee71ec79a9b868a79cd95bf85d97719fdba
= <=>
2021-10-04 14:31:14 -0400

    Exercise 2-9 Contact Selected

--- force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -1,5 +1,11 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api} from 'lwc';
 
 export default class StudentTiles extends LightningElement {
 	@api studentList = [];
+	selectedStudentId = '';
+
+	handleStudentSelected(event){
+		this.selectedStudentId=event.detail.studentId;
+	}
+	
 }
