35c1f63816c68fe66b8b9aa6017ac42ceb6d8694
stylishbharath <bharath.atmakuri@stratos.studio>
2022-03-15 10:31:27 +0530

    Exercise 2-5 Few people

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class StudentBrowser extends LightningElement {}
+export default class StudentBrowser extends LightningElement {
+    constructor() {
+        super();
+        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
+        this.studentList = studentNames.map( (studentName, index) => {
+          return {
+            'sobjectType': 'Contact',
+            'Name': studentName,
+            'PhotoUrl': '/services/images/photo/003B0FakePictId',
+            'Id': index
+          };
+        });
+      }      
+}
