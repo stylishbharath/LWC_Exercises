966fd0e509bb98a50720acf61da904f133d42652
stylishbharath <bharath.atmakuri@stratos.studio>
2022-03-15 12:21:10 +0530

    Exercise 2-7A Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,16 +1,5 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from "lwc";
+import getStudents from "@salesforce/apex/StudentBrowser.getStudents";
 export default class StudentBrowser extends LightningElement {
-    constructor() {
-        super();
-        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
-        this.studentList = studentNames.map( (studentName, index) => {
-          return {
-            'sobjectType': 'Contact',
-            'Name': studentName,
-            'PhotoUrl': '/services/images/photo/003B0FakePictId',
-            'Id': index
-          };
-        });
-      }      
+	@wire(getStudents, { instructorId: "", courseDeliveryId: "" }) students;
 }
