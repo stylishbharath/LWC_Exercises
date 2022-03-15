94423a05512e03c7be81b78d69f0dfa47f5cf318
stylishbharath <bharath.atmakuri@stratos.studio>
2022-03-15 16:29:50 +0530

    Exercise 2-9 Contact Selected

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,16 +1,20 @@
 import { LightningElement, api } from "lwc";
 
 export default class StudentTile extends LightningElement {
+	@api selectedStudentId = "";
 	@api student = {
 		Name: "[John Doe]",
 		PhotoUrl: "/services/images/photo/003B0FakePictId"
 	};
-	@api isSelected = false;
-    get tileSelected() {
-        return this.isSelected ? "tile selected" : "tile";
-    }
-	studentClick(){
-		alert(this.student.Name);
+
+	get tileSelected() {
+		return this.selectedStudentId === this.student.Id ? "tile selected" : "tile";
 	}
 
+	studentClick() {
+		const evt = new CustomEvent("studentselected", {
+			detail: { studentId: this.student.Id }
+		});
+		this.dispatchEvent(evt);
+	}
 }
--- force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -2,5 +2,8 @@ import { LightningElement, api } from 'lwc';
 
 export default class StudentTiles extends LightningElement {
     @api studentList = [];
-    
+    selectedStudentId = '';
+    handleStudentSelected(event){
+        this.selectedStudentId=event.detail.studentId;
+    }
 }
